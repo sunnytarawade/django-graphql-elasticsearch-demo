5624eed6486abd965da814739943e9c1c21d79e6
sunnytarawade <sunnytarawade000@gmail.com>
2022-03-26 16:35:09 +0530

    added default value to env vars

--- api/api/settings.py
+++ api/api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from email.policy import default
 from pathlib import Path
 from decouple import config
 import django
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY',default='')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = config('DEBUG',default=False)
 ALLOWED_HOSTS = ['*']
 
 
